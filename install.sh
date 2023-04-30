45e9d86f16eef68a5d60734a9da3a921664df95b
gligorijevicsava <gligorijevicsava@disroot.org>
2023-04-30 18:01:31 +0200

    adding the /etc/environment file

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48b767e19b13f47d7316844ebf69724aaae0a0cea2faf9dd31356b8cb6b2793c
-size 4816
+oid sha256:87b8c21e2ada695dae5d625815233d28df612b57fabbf3d3f871a4337e4c3bcf
+size 4880
